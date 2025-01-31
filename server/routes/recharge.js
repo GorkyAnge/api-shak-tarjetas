456338c311109e05057bb49d0b0d5d348fbe049b
Gorky Palacios Mutis <gorky.palacios@paymentevolution.com>
2025-01-30 22:53:25 -0500

    Refactor recharge route for consistency and update payment API endpoint

--- server/routes/recharge.js
+++ server/routes/recharge.js
@@ -1,26 +1,29 @@
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
 const router = express.Router();
-const { db } = require('../firebase');
+const { db } = require("../firebase");
 
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   const paymentData = req.body;
   try {
-    const response = await axios.post('https://fake-paymentportal.onrender.com/api/v1/payment/card', paymentData);
+    const response = await axios.post(
+      "https://api-fake-paymentportal-production.up.railway.app/api/v1/payment/card",
+      paymentData
+    );
     const { identifier, amount } = paymentData;
 
     // Actualizar el saldo del identificador
-    const docRef = db.collection('identifiers').doc(identifier);
+    const docRef = db.collection("identifiers").doc(identifier);
     const doc = await docRef.get();
     if (doc.exists) {
       const newBalance = doc.data().balance + parseFloat(amount);
       await docRef.update({ balance: newBalance });
-      res.json({ message: 'Recarga exitosa', newBalance });
+      res.json({ message: "Recarga exitosa", newBalance });
     } else {
-      res.status(404).json({ message: 'Identificador no encontrado' });
+      res.status(404).json({ message: "Identificador no encontrado" });
     }
   } catch (error) {
-    res.status(500).json({ message: 'Error en el pago', error: error.message });
+    res.status(500).json({ message: "Error en el pago", error: error.message });
   }
 });
 
